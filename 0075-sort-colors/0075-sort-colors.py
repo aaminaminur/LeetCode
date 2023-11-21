031b98404c940c93e325c7afbe01e34aa6fabdf0
Aamin <52397046+aaminaminur@users.noreply.github.com>
2023-11-21 21:03:59 +0530

    Time: 43 ms (43.76%), Space: 16.3 MB (9.01%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,20 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def swap(pos1, pos2):
-            nums[pos1], nums[pos2] = nums[pos2], nums[pos1]
-
+        
+        def swap(index1, index2):
+            nums[index1], nums[index2] = nums[index2], nums[index1]
+            
+        
         size = len(nums)
         low = 0
         mid = 0
-        high = size - 1
+        high = size- 1
         while mid <= high:
             if nums[mid] == 0:
-                swap(low, mid)
-                low += 1
-                mid += 1
+                swap(mid, low)
+                mid+=1
+                low+=1
             elif nums[mid] == 1:
-                mid += 1
+                mid+=1
             else:
                 swap(mid, high)
                 high -= 1
+        
