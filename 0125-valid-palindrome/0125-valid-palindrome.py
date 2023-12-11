01cf43a4b3fb95ec9130fb167ba97ae1cbc653e3
Aamin <52397046+aaminaminur@users.noreply.github.com>
2023-12-12 01:23:22 +0530

    Time: 74 ms (6.70%), Space: 17.1 MB (51.85%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,23 +1,23 @@
-import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        asciilist = [ord('a')]
-        for i in range(1, 26):
-            asciilist.append(ord('a')+i)
-        for i in range(0,10):
-            asciilist.append(ord('0')+i)
+        
+        def isalnum(c: chr)-> bool:
+            return (ord('A') <= ord(c) <= ord('Z') or 
+                    ord('0')<= ord(c) <= ord('9') or 
+                    ord('a')<= ord(c) <= ord('z'))
+        
         left = 0
-        right = len(s)-1
-        while(left<right):
-            if ord(s[left]) not in asciilist:
+        right = len(s) - 1
+        while left <= right:
+            if not isalnum(s[left]):
                 left += 1
                 continue
-            if ord(s[right]) not in asciilist:
+            if not isalnum(s[right]):
                 right -= 1
                 continue
-            if s[left] != s[right]:
+            if s[left].lower() != s[right].lower():
                 return False
             left += 1
             right -= 1
+        
         return True
