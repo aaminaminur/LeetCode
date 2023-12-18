7516151a86fef1981ac89270f8a3fff78dc911e7
Aamin <52397046+aaminaminur@users.noreply.github.com>
2023-12-19 00:39:50 +0530

    Time: 41 ms (66.61%), Space: 16.3 MB (37.62%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,24 +1,23 @@
 class Solution:
-    def transpose(self, matrix: List[List[int]]):
-        for i in range(len(matrix)):
-            for j in range(i+1):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
-    def reverse_rows(self, row: List[int]):
-        low = 0
-        high = len(row) - 1
-        while low<high:
-            row[low], row[high] = row[high], row[low]
-            low += 1
-            high -= 1
-        return row
-    
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # get the transpose
-        self.transpose(matrix)
-        # reverse the rows
-        for i in range(len(matrix)):
-            matrix[i] = self.reverse_rows(matrix[i])
+        
+        def reverse_list(data):
+            i = 0
+            j = len(data) - 1
+            while i<=j:
+                data[i], data[j] = data[j], data[i]
+                i += 1
+                j -= 1 
+        
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        for i in range(m):
+            for j in range(i+1):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        for data in matrix:
+            reverse_list(data)
